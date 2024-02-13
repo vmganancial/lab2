65ceeb502c4e1b7f96f4a7ca4ac395c5a4d352b7
Victor Angelo Ganancial <152581228+vmganancial@users.noreply.github.com>
2024-02-13 11:51:33 +0800

    Update form.php

--- week8/form.php
+++ week8/form.php
@@ -100,5 +100,41 @@ echo "<br>";
 echo $gender;
 ?>
 
+<?php
+if ($_Server["REQUEST_METHOD"] == "POST")
+{
+// For Xampp Localhost
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "myDB";
+
+// For socitcloud
+$servername = "localhost";
+$username = "webprogmi221";
+$password = "g_6bCitLu.ljMK*m";
+$dbname = "webprogmi221";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO rlibudan_myguests (name,email,website,comment,gender)
+VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+}
+?>
+
 </body>
 </html>
