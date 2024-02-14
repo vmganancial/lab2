510dfc6f202447e30c04d26da90db4181288580f
Victor Angelo Ganancial <152581228+vmganancial@users.noreply.github.com>
2024-02-15 00:18:06 +0800

    Update form.php

--- week8/form.php
+++ week8/form.php
@@ -120,7 +120,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO vmganancial_myguests (name,email,website,comment,gender)
+$sql = "INSERT INTO vmganancial_myguests (name, email, website, comment, gender)
 VALUES ('$name', '$email', '$website', '$comment', '$gender')";
 
 
