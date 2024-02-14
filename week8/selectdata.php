d53bc44437c538d79737605862c57cc122764353
vmganancial <vmganancial@student.apc.edu.ph>
2024-02-15 00:09:12 +0800

    hi

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
+$username = "webprogmi221";
+$password = "g_6bCitLu.ljMK*m";
+$dbname = "webprogmi221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM MyGuests";
+$sql = "SELECT id, name, email, website, comment, gender FROM vmganancial_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
